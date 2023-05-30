f6f042b808a112ac9e54c2b5b95da8ce294abfd6
ZGY <634712442@qq.com>
2023-05-31 00:00:56 +0800

    update week4 code

--- 250-GoingZ/week4.py
+++ 250-GoingZ/week4.py
@@ -1,61 +1,62 @@
 # 第四周作业
+# 1.实现椒盐噪声
+# 2.实现高斯噪声
+# 3.实现PCA（detail）
 
-#1、 临近插值算法
-#2、 双线性插值算法
-#3、 几何中心对称系数证明
-#4、 直方图均衡化
-
-import cv2
 import numpy as np
+import cv2
+from numpy import shape
+import random
 
-# 1、 临近插值算法
-def nearest_interpolation(img, dstH, dstW):
-    scrH, scrW, _ = img.shape
-    retimg = np.zeros((dstH, dstW, 3), dtype=np.uint8)
-    for i in range(dstH - 1):
-        for j in range(dstW - 1):
-            scrx = round(i * (scrH / dstH))
-            scry = round(j * (scrW / dstW))
-            retimg[i, j] = img[scrx, scry]
-    return retimg
+#### 1. 椒盐噪声 ####
+def SaltNoise(percetage = 0.3):
+    img = cv2.imread('lenna.png')
+    NoiseNum = int(percetage * img.shape[0] * img.shape[1])
+    for i in range(NoiseNum):
+        randX = random.randint(0, img.shape[0]-1)
+        randY = random.randint(0, img.shape[1]-1)
+        if random.random() <= 0.5:
+            img[randX, randY] = 0
+        else:
+            img[randX, randY] = 255
+    return img
 
-# 2、 双线性插值算法
-def bilinear_interpolation(img, dstH, dstW):
-    scrH, scrW, _ = img.shape
-    retimg = np.zeros((dstH, dstW, 3), dtype=np.uint8)
-    for i in range(dstH - 1):
-        for j in range(dstW - 1):
-            scrx = i * (scrH / dstH)
-            scry = j * (scrW / dstW)
-            x = int(scrx)
-            y = int(scry)
-            u = scrx - x
-            v = scry - y
-            retimg[i, j] = (1 - u) * (1 - v) * img[x, y] + u * (1 - v) * img[x + 1, y] + (1 - u) * v * img[x, y + 1] + u * v * img[x + 1, y + 1]
-    return retimg
+#### 2. 高斯噪声 ####
+def GaussianNoise(means = 0, sigma = 4, percetage = 0.3):
+    img = cv2.imread('lenna.png')
+    NoiseNum = int(percetage * img.shape[0] * img.shape[1])
+    for i in range(NoiseNum):
+        randX = random.randint(0, img.shape[0]-1)
+        randY = random.randint(0, img.shape[1]-1)
+        img[randX, randY] = img[randX, randY] + random.gauss(means, sigma)
+        if img[randX, randY] < 0:
+            img[randX, randY] = 0
+        elif img[randX, randY] > 255:
+            img[randX, randY] = 255
+    return img
 
-# 4、 直方图均衡化
-def hist_equal(img):
-    scrH, scrW, _ = img.shape
-    retimg = np.zeros((scrH, scrW, 3), dtype=np.uint8)
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    dst = cv2.equalizeHist(gray)
-    hist = cv2.calcHist([dst], [0], None, [256], [0, 256])
+#### 3. PCA ####
+class PCA():
+    def __init__(self,n_components):
+        self.n_components = n_components
     
-    return np.hstack([gray, dst])
-
-if __name__ == '__main__':
-    img = cv2.imread("lenna.png")
-    dstH = 700
-    dstW = 700
-    # nearest_interpolation
-    ret = nearest_interpolation(img, dstH, dstW)
-    cv2.imshow("nearest_interpolation", ret)
-    # bilinear_interpolation
-    ret = bilinear_interpolation(img, dstH, dstW)
-    cv2.imshow("bilinear_interpolation", ret)
-    # hist_equal
-    ret = hist_equal(img)
-    cv2.imshow("hist_equal", ret)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+    def fit_transform(self,X):
+        self.n_features_ = X.shape[1]
+        # 求协方差矩阵
+        X = X - X.mean(axis=0)
+        self.covariance = np.dot(X.T,X)/X.shape[0]
+        # 求协方差矩阵的特征值和特征向量
+        eig_vals,eig_vectors = np.linalg.eig(self.covariance)
+        # 获得降序排列特征值的序号
+        idx = np.argsort(-eig_vals)
+        # 降维矩阵
+        self.components_ = eig_vectors[:,idx[:self.n_components]]
+        # 对X进行降维
+        return np.dot(X,self.components_)
+ 
+# 调用
+def PCA_test():
+    pca = PCA(n_components=2)
+    X = np.array([[-1,2,66,-1], [-2,6,58,-1], [-3,8,45,-2], [1,9,36,1], [2,10,62,1], [3,5,83,2]])  #导入数据，维度为4
+    newX=pca.fit_transform(X)
+    print(newX)                  #输出降维后的数据
