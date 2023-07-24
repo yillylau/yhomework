aee56f09827aa57823da4e223c1ff70829cc1ca8
weixing2023 <125475746+weixing2023@users.noreply.github.com>
2023-07-24 17:22:45 +0800

    Add files via upload

--- 103-魏星/week11/alexnet/predict.py
+++ 103-魏星/week11/alexnet/predict.py
@@ -10,6 +10,7 @@ if __name__ == "__main__":
     model.load_state_dict(torch.load("./torch-AlexNet.h5"))
 
     img1 = cv2.imread("./Test.jpg")
+    img1 = cv2.resize(img1,(224,224))
     outputs = model.forward(torch.tensor(np.float32(img1.reshape(-1, 3, 224, 224))))
     _, img1_result = torch.max(outputs.data, 1)
     print("模型预测结果是：", img1_result.item())
