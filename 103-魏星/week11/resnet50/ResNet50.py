777ed6c07b0d9c0b3b5c05bea1254f72fa4f6372
weixing2023 <125475746+weixing2023@users.noreply.github.com>
2023-07-24 17:51:59 +0800

    Add files via upload

---
+++ 103-魏星/week11/resnet50/ResNet50.py
@@ -0,0 +1,12 @@
+# _*_ coding : utf-8 _*_
+# @Time : 2023/7/24 17:48
+# @Author : weixing
+# @FileName : netWork
+# @Project : cv
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class ResNet50(nn.Module):
+    pass
