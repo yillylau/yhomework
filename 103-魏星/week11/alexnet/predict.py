d0e8a308c63f53df7ef205974d250e3e9934dd3d
weixing2023 <125475746+weixing2023@users.noreply.github.com>
2023-07-24 17:03:23 +0800

    Add files via upload

---
+++ 103-魏星/week11/alexnet/predict.py
@@ -0,0 +1,17 @@
+import numpy as np
+import cv2
+import torch
+
+from AlexNet import AlexNet
+
+if __name__ == "__main__":
+    print("torch mnist begin")
+    model = AlexNet(num_classes=10)
+    model.load_state_dict(torch.load("./torch-AlexNet.h5"))
+
+    img1 = cv2.imread("./Test.jpg")
+    outputs = model.forward(torch.tensor(np.float32(img1.reshape(-1, 3, 224, 224))))
+    _, img1_result = torch.max(outputs.data, 1)
+    print("模型预测结果是：", img1_result.item())
+
+    print("torch mnist end")
