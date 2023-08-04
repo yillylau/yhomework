40a3ad671116405df8916022d3eba52303727531
weixing2023 <125475746+weixing2023@users.noreply.github.com>
2023-08-04 09:44:44 +0800

    Add files via upload

---
+++ 103-魏星/week12/mobilenet/MobileNetV1.py
@@ -0,0 +1,12 @@
+# _*_ coding : utf-8 _*_
+# @Time : 2023/8/2 15:07
+# @Author : weixing
+# @FileName : MobileNet
+# @Project : cv
+
+import torch.nn as nn
+
+
+class MobileNetV1(nn.Module):
+    pass
+
