8f840650e28013ba0c4636a3c43cd60bd1e01f60
weixing2023 <125475746+weixing2023@users.noreply.github.com>
2023-08-08 16:03:25 +0800

    Add files via upload

---
+++ 103-魏星/week13/config/Config.py
@@ -0,0 +1,10 @@
+# _*_ coding : utf-8 _*_
+# @Time : 2023/8/7 15:06
+# @Author : weixing
+# @FileName : config
+# @Project : cv
+
+class Config:
+    def __init__(self):
+        self.ratios = [0.5, 1, 2]
+        self.anchor_scales = [8, 16, 32]
