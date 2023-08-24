3e5c73df995f3b52408b8ee09144f351563fbdfa
yjyu1 <129635992+yjyu1@users.noreply.github.com>
2023-08-24 14:07:07 +0800

    Add files via upload

---
+++ 63-于钧旭-苏州/第十五周/predict.py
@@ -0,0 +1,15 @@
+from mask_rcnn import MASK_RCNN
+from PIL import Image
+
+mask_rcnn = MASK_RCNN()
+
+while True:
+    img = input('img/street.jpg')
+    try:
+        image = Image.open('img/street.jpg')
+    except:
+        print('Open Error! Try again!')
+        continue
+    else:
+        mask_rcnn.detect_image(image)
+mask_rcnn.close_session()
